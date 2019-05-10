f9bb4f3093e0c19aaffb7065badfd6379055f81f
github username <github email address>
2019-05-10 14:16:36 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        #line directly below is saying if their is grade key inside of hash then do not assign value to empty array.
+        #Next second line pushes student_name into array that was created or already existed.
+        roster[grade] ||= []
+        roster[grade] << student_name
+    end
+
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+
+end
